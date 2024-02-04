9ef7aa6ce40e54d4a6bc205b79e361319d3d3d46
Harikrishna A <Harikichus2018@gmail.com>
2024-02-04 12:41:41 +0530

    Added Events Background

--- sections/Events/Events.jsx
+++ sections/Events/Events.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./Events.css"
 
 const Events = () => {
-  return <div id="Events">Events</div>
+  return (<div id="Events" className="Events-Main-Container">Events</div>)
 }
 
 export default Events
